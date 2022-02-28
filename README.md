86205c36c4000b8981b0378c852c839e73e4fee7
Kyle Rego <regoky@outlook.com>
2022-02-28 15:58:56 -0500

    Save work

---
+++ README.md
@@ -0,0 +1,1 @@
+# A practice Sinatra todo app
--- todo.rb
+++ todo.rb
@@ -1,7 +1,57 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/content_for"
 require "tilt/erubis"
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get '/' do
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+get "/lists/:id" do
+  @list = session[:lists].select { |list| list[:id] == params[:id] }.first
+  erb :list, layout: :layout
+end
+
+# Return an error message if the name is invalid. Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique."
+  end
+end
+
+current_list_id = 0
+
+post "/lists" do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [], id: current_list_id.to_s }
+    current_list_id += 1
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
 end

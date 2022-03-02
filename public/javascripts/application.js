6b14446ce2b9458aed619331289bd4041a2c1bfd
Kyle Rego <regoky@outlook.com>
2022-03-02 16:22:32 -0500

    Ask user to confirm before deleting a todo or list

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,13 @@
 $(function() {
-  console.log('the page loaded')
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? This cannot be undone!");
+    if (ok) {
+      this.submit();
+    }
+  });
+
 });
